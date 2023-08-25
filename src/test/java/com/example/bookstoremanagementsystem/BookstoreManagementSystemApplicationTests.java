424d468763a2fab9b46b0ad1dd28031e9ea5175e
ousmane <sangaryousmane@gmail.com>
2023-08-25 19:37:01 +0800

    Initial files

---
+++ src/test/java/com/example/bookstoremanagementsystem/BookstoreManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookstoremanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
